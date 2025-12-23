8234c3915807a132c62e737395d4935a235bfe82
Mayia Rodina <designw657@gmail.com>
2025-12-23 23:50:46 +0300

    fix geo

--- src/js/message/message.js
+++ src/js/message/message.js
@@ -8,27 +8,30 @@ export class Message {
   }
 
   async getGeo() {
-  try {
-    if (!navigator.geolocation) {
-      throw new Error('no-geolocation');
-    }
-
-    this.geo = await new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition((value) => resolve(value.coords), reject, {
-        enableHighAccuracy: true,
-        timeout: 5000,
-      });
-    });
-
-    return;
-  } catch (err) {
-    console.warn('Geolocation failed:', err.message);
-
     try {
-      this.geo = await this.modal.getCoordinatsFromUser();
-    } catch {
-      console.warn('User cancelled manual input');
+      if (!navigator.geolocation) {
+        throw new Error('no-geolocation');
+      }
+
+      this.geo = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition((value) => resolve(value.coords), reject, {
+          enableHighAccuracy: true,
+          timeout: 5000,
+        });
+      });
+
+      return;
+    } catch (err) {
+      console.warn('Geolocation failed:', err.message);
+
+      try {
+        this.geo = await this.modal.getCoordinatsFromUser();
+        return;
+      } catch {
+        console.warn('User cancelled manual input');
+      }
+
+      this.geo = [0, 0];
     }
   }
-}
 }
